a7a43bd385f9991402274b9dd1e9844b3cdd7a07
Fábio Aguiar <fabioaguiar.ti@gmail.com>
2023-04-24 14:42:06 +0000

    Alteração no nome dos metodos de plot / Alteração no diretório dos arquivos de plot

--- jobs/etl_job.py
+++ jobs/etl_job.py
@@ -4,11 +4,9 @@ etl_job.py
 Script para execução do job Spark:
     $SPARK_HOME/bin/spark-submit \
         --master local[*] \
-        --py-files packages.zip $HOME/projects/pyspark-project-vgsales/jobs/etl_job.py
+        --py-files packages.zip $HOME/projects/pyspark-video-game-analysis/jobs/etl_job.py
 """
 import pyspark.pandas as pspd
-import numpy as np
-import pandas as pd
 
 from dependencies.spark import start_spark
 
@@ -21,14 +19,14 @@ def main():
 
     # Execução do pipeline ETL
     data = extract_data(spark)
-    #data_transformed = transform_data(data, "SUM_GLOBAL_SALES", "CHART_PIE")
+    #data_transformed = transform_data(data, "COUNT_GENRES", plataforms)
     plataforms = data['Platform'].unique().to_list()
-    load_charts_pie_1(transform_data(data, "COUNT_GENRES"), plataforms, "COUNT_GENRES")
-    load_charts_pie_2(transform_data(data, "SUM_GLOBAL_SALES"), plataforms, "SUM_GLOBAL_SALES")
-    load_charts_bar_1(transform_data(data, "COUNT_GENRES"), plataforms) 
-    load_charts_bar_2(transform_data(data, "SUM_GLOBAL_SALES"), plataforms) 
-    load_charts_bar_3(transform_data(data, "COUNT_GENRES_YEAR"), plataforms) 
-    load_charts_bar_4(transform_data(data, "COUNT_GAMES_YEAR"), plataforms) 
+    load_plot_pie_1(transform_data(data, "COUNT_GENRES"), plataforms, "COUNT_GENRES")
+    load_plot_pie_2(transform_data(data, "SUM_GLOBAL_SALES"), plataforms, "SUM_GLOBAL_SALES")
+    load_plot_bar_1(transform_data(data, "COUNT_GENRES"), plataforms) 
+    load_plot_bar_2(transform_data(data, "SUM_GLOBAL_SALES"), plataforms) 
+    load_plot_bar_3(transform_data(data, "COUNT_GAMES_YEAR"), plataforms) 
+
 
     spark.stop()
     return None
@@ -48,8 +46,6 @@ def transform_data(df, chart_metric):
         df_transformed = pspd.DataFrame(df_transformed.sort_values(by=['Global_Sales'], ascending=False).groupby(['Platform', 'Genre'])['Global_Sales'].sum())
     elif (chart_metric == "COUNT_GENRES"):
         df_transformed = pspd.DataFrame(df_transformed.sort_values(by=['Genre'], ascending=False).groupby(['Platform', 'Genre'])['Genre'].count())
-    elif (chart_metric == "COUNT_GENRES_YEAR"):
-        df_transformed = pspd.DataFrame(df_transformed.sort_values(by=['Genre'], ascending=False).groupby(['Platform', 'Genre', 'Year'])['Genre'].count())
     elif (chart_metric == "COUNT_GAMES_YEAR"):
         df_transformed = pspd.DataFrame(df_transformed.sort_values(by=['Genre'], ascending=False).groupby(['Platform', 'Year'])['Genre'].count())
 
@@ -63,65 +59,56 @@ def round_value_chart(df, chart_metric):
 
     return df
 
-def load_charts_pie_1(df, plataforms, chart_metric):
+def load_plot_pie_1(df, plataforms, chart_metric):
     for platform in plataforms:
         pie_df = df.xs(platform,level=0)
         pie_df = round_value_chart(pie_df, chart_metric) #Arredonda registros do gráfico retirando os menores valores
         plot = pie_df.to_pandas().plot.pie(y='Genre', figsize=(10,10), title=("Contagem de jogos por gênero: " + platform), fontsize=16, pctdistance=0.9, autopct='%1.1f%%',legend=False)
         fig = plot.get_figure()
-        fig.savefig("../pyspark-project-vgsales/data/charts/"+"Pie_Count_Genre_per_platform_percent_"+platform+".png")
+        fig.savefig("../pyspark-video-game-analysis/data/plots/"+"Pie_Count_Genre_percent_"+platform+".png")
 
     return None
 
 
-def load_charts_pie_2(df, plataforms, chart_metric):
+def load_plot_pie_2(df, plataforms, chart_metric):
     for platform in plataforms:
         pie_df = df.xs(platform,level=0)
         pie_df = round_value_chart(pie_df, chart_metric) #Arredonda registros do gráfico retirando os menores valores
         plot = pie_df.to_pandas().plot.pie(y='Global_Sales', figsize=(10,10), title=("Vendas Globais por gênero: " + platform), fontsize=16, pctdistance=0.9, autopct='%1.1f%%',legend=False)
         fig = plot.get_figure()
-        fig.savefig("../pyspark-project-vgsales/data/charts/"+"Pie_Global_Sales_Genre_per_platform_percent_"+platform+".png")
+        fig.savefig("../pyspark-video-game-analysis/data/plots/"+"Pie_Global_Sales_Genre_percent_"+platform+".png")
 
     return None
 
-def load_charts_bar_1(df, plataforms):
+def load_plot_bar_1(df, plataforms):
     for platform in plataforms:
         pie_df = df.xs(platform,level=0)
         plot = pie_df.to_pandas().plot.pie(y='Genre', figsize=(10,10), title=("Contagem de jogos por gênero: " + platform), fontsize=16, pctdistance=0.9, autopct='%1.1f%%',legend=False)
         fig = plot.get_figure()
-        fig.savefig("../pyspark-project-vgsales/data/charts/"+"Bar_Count_Genre_per_platform_percent_"+platform+".png")
+        fig.savefig("../pyspark-video-game-analysis/data/plots/"+"Bar_Count_Genre_percent_"+platform+".png")
 
     return None
 
-def load_charts_bar_2(df, plataforms):
+def load_plot_bar_2(df, plataforms):
     for platform in plataforms:
         pie_df = df.xs(platform,level=0)
         plot = pie_df.to_pandas().plot.bar(y='Global_Sales', figsize=(10,10), title=("Vendas Globais por gênero: " + platform), fontsize=16, legend=False)
         fig = plot.get_figure()
-        fig.savefig("../pyspark-project-vgsales/data/charts/"+"Bar_Global_Sales_Genre_per_platform"+platform+".png")
+        fig.savefig("../pyspark-video-game-analysis/data/plots/"+"Bar_Global_Sales_Genre_"+platform+".png")
 
     return None
 
-def load_charts_bar_3(df, plataforms):
-    for platform in plataforms:
-        pie_df = df.xs(platform,level=0)
-        pie_df = pie_df.sort_index(0, ascending=True,level=[1,0])        
-        plot = pie_df.to_pandas().plot.bar(y='Genre', figsize=(10,10), title=("Contagem de jogos vendidos por ano: " + platform), fontsize=16, legend=False)
-        fig = plot.get_figure()
-        fig.savefig("../pyspark-project-vgsales/data/charts/"+"Bar_count_Genre_per_year"+platform+".png")
-
-    return None
-
-
-def load_charts_bar_4(df, plataforms):
+def load_plot_bar_3(df, plataforms):
     for platform in plataforms:
         pie_df = df.xs(platform,level=0)
         pie_df = pie_df.sort_index(0, ascending=True,level=0)
         plot = pie_df.to_pandas().plot.bar(y='Genre', figsize=(10,10), title=("Contagem de jogos vendidos por ano: " + platform), fontsize=16, legend=False)
         fig = plot.get_figure()
-        fig.savefig("../pyspark-project-vgsales/data/charts/"+"Bar_count_Games_per_year"+platform+".png")
+        fig.savefig("../pyspark-video-game-analysis/data/plots/"+"Bar_count_Genre_per_year"+platform+".png")
 
     return None
+
+
 
 # entry point for PySpark ETL application
 if __name__ == '__main__':
